adba9fb972d9e4dc768a1fe6db7567d2c02d5435
altamiranoErica <altamirano.erica@gmail.com>
2014-04-06 20:24:55 -0300

    Implementado los dos primeros metodos de estadisticas.

--- tp1-mixins-complejodeportivo/src/main/ComplejoDeportivo.scala
+++ tp1-mixins-complejodeportivo/src/main/ComplejoDeportivo.scala
@@ -3,6 +3,7 @@ package main
 import scala.collection.mutable.ArrayBuffer
 import canchas.Cancha
 import org.joda.time.DateTime
+import canchas.Reserva
 
 class ComplejoDeportivo {
   var canchas : ArrayBuffer[Cancha] = new ArrayBuffer[Cancha] 
@@ -11,4 +12,12 @@ class ComplejoDeportivo {
     
   }
   
+  def canchasConReservaElDia(dia : DateTime) : ArrayBuffer[Cancha] = {
+    canchas filter (_.reservas exists (_.dia.equals(dia)))
+  }
+  
+  def reservasDelDia(dia : DateTime) : ArrayBuffer[Reserva] = {
+    this.canchasConReservaElDia(dia).map(_.reservas).fold(new ArrayBuffer[Reserva])((r, rs) => r ++ rs)
+  }
+
 }
