991b8b8d0ad58becc237d8da9f50257553267a7c
Marcelo <rubini.marcelo@gmail.com>
2014-06-16 01:09:57 -0300

    Subo los tests de todas maneras para no perder la linea conceptual

--- tp4-dsls-internos-agenda/test/dominio/AgendaTest.java
+++ tp4-dsls-internos-agenda/test/dominio/AgendaTest.java
@@ -12,45 +12,9 @@ public class AgendaTest {
 	
 	
 	@Test
-	public void tickingTest() {
+	public void testTick() {
 		
 	}
 	
-	
-	@Test
-	public void obtenerEventoTest() {
-		
-	}
-	@Test
-	public void obtenerEventoInexistenteTest() {
-		
-	}
-	
-	@Test
-	public void agregarEventoNuevoTest() {
-
-	}
-	@Test
-	public void agregarEventoRepetidoTest() {
-
-	}
-	
-	@Test
-	public void removerEventoTest() {
-
-	}
-	@Test
-	public void removerEventoInexistenteTest() {
-
-	}
-
-	@Test
-	public void cancelarEventoTest() {
-		
-	}
-	@Test
-	public void cancelarEventoInexistenteTest() {
-		
-	}
 	
 }
--- tp4-dsls-internos-agenda/test/dominio/NotificadorTest.java
+++ tp4-dsls-internos-agenda/test/dominio/NotificadorTest.java
@@ -1,14 +1,56 @@
 package dominio;
 
-import static org.junit.Assert.*;
+import org.junit.*;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class NotificadorTest {
 
+	Evento mockedEvento;
+	
+	Recordatorio mockedRecordT;
+	Recordatorio mockedRecordS;
+	Recordatorio mockedRecordM;
+	
+	Notificador notificador;
+	
+	
+	@Before
+	public void setUp() {
+		mockedEvento = mock(Evento.class);
+		
+		mockedRecordT = mock(RecordatorioTelefonico.class);
+		mockedRecordS = mock(RecordatorioSMS.class);
+		mockedRecordM = mock(RecordatorioEmail.class);
+		
+		notificador = new Notificador();
+	}
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testNotificarViaEmail() {
+		notificador.notificarViaMail(mockedRecordM);
+		assertTrue(notificador.getLogRecordatorios().contains(mockedRecordM));
+	}
+	
+	@Test
+	public void testNotificarViaSMS() {
+		notificador.notificarViaSMS(mockedRecordS);
+		assertTrue(notificador.getLogRecordatorios().contains(mockedRecordS));
+	}
+	
+	@Test
+	public void testNotificarViaLlamada() {
+		notificador.notificarViaLlamada(mockedRecordT);
+		assertTrue(notificador.getLogRecordatorios().contains(mockedRecordT));
 	}
 
+	@Test
+	public void testSucedio() {
+		//Profundizar el test y hacer que el evento dispare notificarViaALGO
+		notificador.sucedio(mockedEvento);
+		verify(mockedEvento, times(1)).notificarUsuario(notificador);
+		
+	}
 }
